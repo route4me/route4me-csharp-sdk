b748774b6c76f0d4ddbd6187490b9bf0786080ee
Vic <victor.zyabko@gmail.com>
2015-07-02 00:23:04 -0700

    Extended error handling.

---
+++ Route4MeSDKLibrary/DataTypes/ErrorResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route4MeSDKLibrary.DataTypes
+{
+    /// <summary>
+    /// Errors data-structure
+    /// </summary>
+    [DataContract]
+    public class ErrorResponse
+    {
+        [DataMember(Name = "errors")]
+        public List<String> Errors { get; set; }
+    }
+}
