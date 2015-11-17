b5269b4208b16f747513ecd4f2197716902336c1
Route4Me <novapulsar@users.noreply.github.com>
2015-11-17 15:47:27 -0500

    Update TrackingHistory.cs

--- Route4MeSDKLibrary/DataTypes/TrackingHistory.cs
+++ Route4MeSDKLibrary/DataTypes/TrackingHistory.cs
@@ -2,28 +2,32 @@
 
 namespace Route4MeSDK.DataTypes
 {
-  /// <summary>
-  /// Tracking history data-structure
-  /// See https://www.assembla.com/spaces/route4me_api/wiki/Route_v4
-  /// </summary>
+  
   [DataContract]
   public sealed class TrackingHistory
   {
+    /* tracking data key names are shortened to reduce bandwidth usage (even with compression on) */
+    // speed at the time of the location transaction event
     [DataMember(Name = "s")]
     public double? Speed {get; set; }
 
+    // latitude at the time of the location transaction event
     [DataMember(Name = "lt")]
     public double? Latitude { get; set; }
-
+    
+    // longitude at the time of the location transaction event
     [DataMember(Name = "lg")]
     public double? Longitude { get; set; }
 
+    // direction/heading at the time of the location transaction event
     [DataMember(Name = "d")]
     public string D {get; set; }
 
+    // the original timestamp in unix timestamp format at the moment location transaction event
     [DataMember(Name = "ts")]
     public string TimeStamp {get; set; }
-
+    
+    // the original timestamp in a human readable timestamp format at the moment location transaction event
     [DataMember(Name = "ts_friendly")]
     public string TimeStampFriendly { get; set; }
   }
