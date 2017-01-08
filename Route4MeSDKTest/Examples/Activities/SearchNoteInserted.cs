bfde82db56d78fda3b1820c4d0b4f92bbde1246e
Amazos <olegi_g@yahoo.com>
2017-01-08 20:45:29 +0400

    added SearchNoteInserted example

---
+++ Route4MeSDKTest/Examples/Activities/SearchNoteInserted.cs
@@ -0,0 +1,48 @@
+﻿using Route4MeSDK.DataTypes;
+using Route4MeSDK.QueryTypes;
+using System;
+
+namespace Route4MeSDK.Examples
+{
+    public sealed partial class Route4MeExamples
+    {
+        /// <summary>
+        /// Get Activities Note Inserted
+        /// </summary>
+        public void SearchNoteInserted()
+        {
+            // Create the manager with the api key
+            Route4MeManager route4Me = new Route4MeManager(c_ApiKey);
+
+            ActivityParameters activityParameters = new ActivityParameters
+            {
+                ActivityType = "note-insert",
+                RouteId = "C3E7FD2F8775526674AE5FD83E25B88A"
+            };
+
+            // Run the query
+            string errorString = "";
+            Activity[] activities = route4Me.GetActivityFeed(activityParameters, out errorString);
+
+            Console.WriteLine("");
+
+            if (activities != null)
+            {
+                Console.WriteLine("SearchNoteInserted executed successfully, {0} activities returned", activities.Length);
+                Console.WriteLine("");
+
+                foreach (Activity Activity in activities)
+                {
+                    Console.WriteLine("Activity ID: {0}", Activity.ActivityId);
+                }
+
+                Console.WriteLine("");
+            }
+            else
+            {
+                Console.WriteLine("SearchNoteInserted error: {0}", errorString);
+            }
+
+        }
+    }
+}
