a2ee85cb3137acbc2524a905ad0fa7e4e8c129f7
Amazos <olegi_g@yahoo.com>
2017-01-09 18:36:31 +0400

    added SearchRoutedLocations example

--- Route4MeSDKTest/shortTest.cs
+++ Route4MeSDKTest/shortTest.cs
@@ -14,8 +14,12 @@ namespace Route4MeSDKTest
 
             // Note run an example by uncommenting code lines between appropriate //=======.... lines
 
+            // ======== Search Routed Locations  ===========================
+              examples.SearchRoutedLocations();
+            // ======================================================================
+
             // ======== Search Locations By IDs  ===========================
-             examples.SearchLocationsByIDs();
+            // examples.SearchLocationsByIDs();
             // ======================================================================
 
             // ======== Get Addressbook Specified Fields Filtered by Text in Any Field  ===========================
