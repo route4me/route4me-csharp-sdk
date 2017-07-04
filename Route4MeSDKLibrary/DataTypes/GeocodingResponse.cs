b1ef6fcf8c91d3ecb83d8ea7574bcb3989edc868
Amazos <olegi_g@yahoo.com>
2017-07-04 19:51:35 +0400

    added GeocodingResponse class

---
+++ Route4MeSDKLibrary/DataTypes/GeocodingResponse.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace Route4MeSDK.DataTypes
+{
+    [DataContract]
+    public sealed class GeocodingResponse
+    {
+        [DataMember(Name = "address", EmitDefaultValue = false)]
+        public string address { get; set; }
+
+        [DataMember(Name = "lat", EmitDefaultValue = false)]
+        public double lat { get; set; }
+
+        [DataMember(Name = "lng", EmitDefaultValue = false)]
+        public double lng { get; set; }
+
+        [DataMember(Name = "type", EmitDefaultValue = false)]
+        public string type { get; set; }
+
+        [DataMember(Name = "confidence", EmitDefaultValue = false)]
+        public string confidence { get; set; }
+
+        [DataMember(Name = "original", EmitDefaultValue = false)]
+        public string original { get; set; }
+    }
+}
