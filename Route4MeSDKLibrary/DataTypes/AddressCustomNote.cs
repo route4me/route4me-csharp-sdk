b63be08af8315516e3382d10603a29fbf4aee083
Amazos <olegi_g@yahoo.com>
2018-08-12 14:39:04 +0400

    updated Custom Note classes

---
+++ Route4MeSDKLibrary/DataTypes/AddressCustomNote.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Route4MeSDK.DataTypes
+{
+    [DataContract]
+    public sealed class AddressCustomNote
+    {
+        [DataMember(Name = "note_custom_entry_id", EmitDefaultValue = false)]
+        public int NoteCustomEntryID { get; set; }
+
+        [DataMember(Name = "note_id", EmitDefaultValue = false)]
+        public string NoteID { get; set; }
+
+        [DataMember(Name = "note_custom_type_id", EmitDefaultValue = false)]
+        public int NoteCustomTypeID { get; set; }
+
+        [DataMember(Name = "note_custom_value")]
+        public string NoteCustomValue { get; set; }
+
+        [DataMember(Name = "note_custom_type")]
+        public string NoteCustomType { get; set; }
+    }
+}
---
+++ Route4MeSDKLibrary/DataTypes/CustomNoteType.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+namespace Route4MeSDK.DataTypes
+{
+    [DataContract]
+    public sealed class CustomNoteType
+    {
+        [DataMember(Name = "note_custom_type_id", EmitDefaultValue = false)]
+        public int NoteCustomTypeID { get; set; }
+
+        [DataMember(Name = "note_custom_type", EmitDefaultValue = false)]
+        public string NoteCustomType { get; set; }
+
+        [DataMember(Name = "root_owner_member_id", EmitDefaultValue = false)]
+        public int RootOwnerMemberID { get; set; }
+
+        [DataMember(Name = "note_custom_type_values")]
+        public string[] NoteCustomTypeValues { get; set; }
+    }
+}
+
