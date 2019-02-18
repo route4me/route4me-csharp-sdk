a6d4c075569777adeb20555654f8d46057a95170
Amazos <olegi_g@yahoo.com>
2019-02-18 19:00:28 +0400

    Create AddressGeocoded.cs

---
+++ Route4MeSDKLibrary/DataTypes/AddressGeocoded.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Route4MeSDK.DataTypes
+{
+    [DataContract]
+    public sealed class AddressGeocoded
+    {
+        [DataMember(Name = "address", EmitDefaultValue = false)]
+        public Address geocodedAddress { get; set; }
+
+        [DataMember(Name = "member_id", EmitDefaultValue = false)]
+        public int MemberId { get; set; }
+
+        [DataMember(Name = "member_type_id", EmitDefaultValue = false)]
+        public int memberTypeID { get; set; }
+
+        [DataMember(Name = "userLat")]
+        public double userLatitude { get; set; }
+
+        [DataMember(Name = "userLng")]
+        public double userLongitude { get; set; }
+
+        [DataMember(Name = "intUserIP", EmitDefaultValue = false)]
+        public uint intUserIP { get; set; }
+
+        [DataMember(Name = "strGeocodingMethod", EmitDefaultValue = false)]
+        public string strGeocodingMethod { get; set; }
+
+        [DataMember(Name = "getCurbside", EmitDefaultValue = false)]
+        public bool getCurbside { get; set; }
+
+        [DataMember(Name = "priority", EmitDefaultValue = false)]
+        public int Priority { get; set; }
+    }
+}
